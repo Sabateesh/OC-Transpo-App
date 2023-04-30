87171ad1b1514cae17c474bfbb2589105e97368a
Sabateesh Sivakumar <80174416+Sabateesh@users.noreply.github.com>
2023-04-30 18:56:18 -0400

    API KEY HIDDEN

--- OCTranspoBusSchedule/AppDelegate.swift
+++ OCTranspoBusSchedule/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyDvkCg1wd25wgt1vgnUP9tMOWkaIVvlOSY")
-        GMSPlacesClient.provideAPIKey("AIzaSyDvkCg1wd25wgt1vgnUP9tMOWkaIVvlOSY")
+        GMSServices.provideAPIKey("NOTPUBLIC")
+        GMSPlacesClient.provideAPIKey("NOTPUBLIC")
         
         return true
     }
